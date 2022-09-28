b5ac56683bc0ed13c716a36d7f11f22683275aed
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-09-28 17:16:57 -0300

    Arquivos necessários criados

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GlobalStyle from "./GlobalStyles";
+import styled from 'styled-components';
+
+export default function App(){
+    return (
+        <>
+            <GlobalStyle/>
+
+            <h1>oi</h1>
+        </>
+    )
+}
